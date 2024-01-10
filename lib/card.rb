238d7ff155e60b313ac3528e88c4e66320e24b19
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-10 13:03:33 -0700

    Initial commit

---
+++ lib/card.rb
@@ -0,0 +1,10 @@
+class Card
+
+  attr_reader :suit, :value, :rank
+
+  initialize(suit, value, rank)
+
+
+
+
+end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,16 +3,23 @@ require './lib/card'
 
 RSpec.describe Card do
   it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:heart, 'Jack', 11)
 
-    expect(card).to be_an_instance_of(Card)
+    expect(card1).to be_an_instance_of(Card)
   end
 
   it "has readable attributes" do
     card = Card.new(:diamond, 'Queen', 12)
 
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+    expect(card1.suit).to eq(:diamond)
+    expect(card1.value).to eq('Queen')
+    expect(card1.rank).to eq(12)
   end
 end
+
+
+
+expect(card.suit).to eq(:heart)
+expect(card.value).to eq("Jack")
+expect(card.rank).to eq(11)
