e07a18e2419622a93c45255e3ed6a380afc47f22
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-17 15:13:49 -0700

    #start

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,26 @@ class Game
     @player2 = player2
   end
 
+  def start
+    puts "Welcome to War! (or Peace) This game will be played with 52 cards."
+    puts "The players today are Megan and Aurora."
+    puts "Type 'go' to start the game!"
+    puts "Enter 'q' to quit."
+    puts "------------------------------------------------------------------"
+    response = gets.downcase.chomp
+    if response == "go"
+      go
+    elsif response == "q"
+      puts "Smell ya later!"
+    else
+      start
+    end
+  end
+
+  def go
+
+  end
+
 
 
 end
